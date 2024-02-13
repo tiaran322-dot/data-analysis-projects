93624756134b2f13a07d9f55d0e29c9276bd0f8b
John Woolbright <jwoolbright23@gmail.com>
2024-02-13 10:21:57 -0600

    how to write code example

---
+++ how-to-write-code/comments.py
@@ -0,0 +1,12 @@
+# This demo shows off comments!
+
+# print("This does not print.");
+
+print("Hello, World!"); # Comments do not have to start at the beginning of a line.
+
+# Here is how
+# to have
+# multi-line
+# comments.
+
+print("Comments make your code more readable by others.");

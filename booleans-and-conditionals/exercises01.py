11d279ea4e52a93f1e277e8896c91a40424f85b9
Sally Steuterman <sally.steuterman@gmail.com>
2024-02-14 16:22:49 -0600

    Booleans starter code

---
+++ booleans-and-conditionals/exercises01.py
@@ -0,0 +1,12 @@
+# Assign the variables for exercise 1 here:
+
+
+
+# BEFORE running the code, predict what will be printed to the console by the following statements:
+
+if engine_indicator_light == "green": 
+  print("engines have started")
+elif engine_indicator_light == "green blinking": 
+  print("engines are preparing to start")
+else:
+  print("engines are off")

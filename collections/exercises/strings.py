cceae23c42ae29663a6b6026fc10cf17f099c71e
tiaran322-dot <tiaran322@gmail.com>
2025-11-10 12:41:43 -0600

    Collections Strings Complete

--- collections/exercises/strings.py
+++ collections/exercises/strings.py
@@ -4,22 +4,30 @@ text = 'Strings_are_sequences_of_characters.'
 word = 'tomato'
 
 # 1. Print a slice of the first 12 characters from 'text'.
-
+print(text[:12])
 
 # 2. Print a slice of the last 12 characters from 'text'. You should NOT have to count the index values yourself!
-
+print(text[-12:])
 
 # 3. Print a slice of the middle 12 characters from 'text'.
-
+print(text[12:24])
 
 # ---- Exercise 3: Looping Through a String ----
 
 # Use index values to loop backwards through 'word'.
 
 # 1. Print 1 letter per line.
-
+for char in word:
+    print(char)
 
 # 2. Refactor the code to use the accumulator pattern to build up and print the reversed string. For example, if given 'good', print 'doog' on one line.
-
+max_index = len(word)-1
+for index in range(max_index, -1, -1):
+   print(word[index])
 
 # 3. Refactor the code to print a combination of the original and reversed string. For example, given 'tomato', print 'tomatootamot'. (If you want to be fancy, print 'tomato | otamot').
+new_word = ""
+for index in range(max_index, -1, -1):
+   new_word += word[index]
+
+print(new_word)

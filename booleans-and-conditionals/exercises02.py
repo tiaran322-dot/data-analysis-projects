5afa6c67744d66c65b3d284019bcc89eb15a0194
tiaran322-dot <tiaran322@gmail.com>
2025-11-10 12:38:14 -0600

    Booleans and Conditionals Part 2 Complete

--- booleans-and-conditionals/exercises02.py
+++ booleans-and-conditionals/exercises02.py
@@ -8,14 +8,37 @@ shuttle_speed = 15000
 # 3) Write conditional expressions to satisfy the following safety rules:
 
 # a) If crew_status is true, print "Crew Ready" else print "Crew Not Ready".
-
+if crew_status == True:
+    print("Crew Ready")
+else:
+    print("Crew Not Ready")
 
 # b) If computer_status_code is 200, print "Please stand by. Computer is rebooting." Else if computer_status_code is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
-
+if computer_status_code == 200:
+    print("Please stand by. Computer is rebooting.")
+elif computer_status_code == 400:
+    print("Success! Computer online.")
+else:
+    print("ALERT: Computer offine!")
 
 # c) If shuttle_speed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttle_speed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
-
+if shuttle_speed > 17500:
+   print("ALERT: Escape velocity reached!")
+elif shuttle_speed < 8000:
+   print("ALERT: Cannot maintain orbit!")
+else:
+   print("Stable speed.")
 
 # 4) PREDICT: Do the code blocks shown in the Section D produce the same result?
+if crew_status and computer_status_code == 200 and space_suits_on:
+   print("all systems go")
+else:
+   print("WARNING. Not ready")
 
+if crew_status != True or computer_status_code != 200 or not(space_suits_on):
+   print("WARNING. Not ready")
+else:
+   print("all systems go")
 # print("Yes" or "No")
+
+print("yes")
